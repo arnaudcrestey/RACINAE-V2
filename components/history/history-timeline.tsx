ff27b21ab7eb5fc6fc759be20144e95dbf804f57
arnaudcrestey <arnaud.crestey14@gmail.com>
2026-06-27 10:21:05 +0200

    Update history-timeline.tsx

--- components/history/history-timeline.tsx
+++ components/history/history-timeline.tsx
@@ -1,19 +1,7 @@
 "use client"
 
-import { useState } from "react"
+import { BookStudio } from "./book-studio"
 
 export function HistoryTimeline() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="p-8">
-      <button
-        type="button"
-        onClick={() => setCount((value) => value + 1)}
-        className="rounded-full bg-black px-6 py-4 text-white"
-      >
-        TEST MOBILE : {count}
-      </button>
-    </div>
-  )
+  return <BookStudio />
 }
